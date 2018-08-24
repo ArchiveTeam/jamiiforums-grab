d3b65c4fda98d752aa1944a73c7933968b2d3d88
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2018-08-25 00:55:33 +0200

    Bump version to 20180824.01

--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20180823.01'
+VERSION = '20180824.01'
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'jamiiforums'
 TRACKER_HOST = 'tracker.archiveteam.org'
